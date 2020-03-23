99bfd3e123041f5a58cd6e03e004a5ad5b904d3f
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-03-23 16:11:23 +0200

    refactor code

--- src/search-flight/flights-gateway.js
+++ src/search-flight/flights-gateway.js
@@ -2,6 +2,6 @@ const baseUrl = 'https://api.iev.aero/api/flights';
 
 export const fetchFlights = async (date) => {
   const res = await fetch(`${baseUrl}/${date}`);
-  if (res.ok) return await res.json();
+  if (res.ok) return res.json();
   throw new Error(`Could not fetch, received ${res.status}`);
 };
